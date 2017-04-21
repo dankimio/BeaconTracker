e3409d0aa9e2a37d67a687f20440ef9dd544450c
Dan Kim <itsdanya@gmail.com>
2017-04-21 05:27:44 +0300

    Range beacons in NewBeaconViewController

--- BeaconTracker/NewBeaconViewController.swift
+++ BeaconTracker/NewBeaconViewController.swift
@@ -7,21 +7,47 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewBeaconViewController: UIViewController {
+
+  let locationManager = CLLocationManager()
+  let region = CLBeaconRegion(
+    proximityUUID: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+    identifier: "identifier"
+  )
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     // Do any additional setup after loading the view.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    locationManager.delegate = self
+    locationManager.startRangingBeacons(in: region)
   }
 
   @IBAction func cancel(_ sender: UIBarButtonItem) {
     performSegue(withIdentifier: "UnwindFromNewBeaconToBeacons", sender: self)
   }
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    guard let passcodeViewController = segue.destination as? PasscodeViewController else { return }
+    guard let beacon = sender as? CLBeacon else { return }
+    passcodeViewController.beacon = beacon
+  }
+
+}
+
+extension NewBeaconViewController: CLLocationManagerDelegate {
+
+  func locationManager(_ manager: CLLocationManager,
+                       didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+    print(beacons)
+
+    guard let beacon = beacons.first else { return }
+    guard beacon.proximity == .immediate else { return }
+
+    performSegue(withIdentifier: "Passcode", sender: beacon)
+    locationManager.stopRangingBeacons(in: region)
+  }
+
 }
