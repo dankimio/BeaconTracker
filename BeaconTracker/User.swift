21301303dca31ce6dfe7f08276c53d3168995beb
Dan Kim <itsdanya@gmail.com>
2017-05-20 16:04:45 +0300

    Add email to User

--- BeaconTracker/User.swift
+++ BeaconTracker/User.swift
@@ -10,17 +10,20 @@ import Foundation
 
 class User: NSObject, NSCoding {
   var id: Int
+  var email: String
   var name: String
   var apiToken: String
 
-  init(id: Int, name: String, apiToken: String) {
+  init(id: Int, email: String, name: String, apiToken: String) {
     self.id = id
+    self.email = email
     self.name = name
     self.apiToken = apiToken
   }
 
   init?(json: [String: Any]) {
     guard let id = json["id"] as? Int,
+      let email = json["email"] as? String,
       let name = json["name"] as? String,
       let apiToken = json["api_token"] as? String
     else {
@@ -28,16 +31,18 @@ class User: NSObject, NSCoding {
     }
 
     self.id = id
+    self.email = email
     self.name = name
     self.apiToken = apiToken
   }
 
   required convenience init?(coder aDecoder: NSCoder) {
     let id = aDecoder.decodeInteger(forKey: "id")
+    let email = aDecoder.decodeObject(forKey: "email") as! String
     let name = aDecoder.decodeObject(forKey: "name") as! String
     let apiToken = aDecoder.decodeObject(forKey: "apiToken") as! String
 
-    self.init(id: id, name: name, apiToken: apiToken)
+    self.init(id: id, email: email, name: name, apiToken: apiToken)
   }
 
   static var current: User? {
@@ -60,6 +65,7 @@ class User: NSObject, NSCoding {
 
   func encode(with aCoder: NSCoder) {
     aCoder.encode(id, forKey: "id")
+    aCoder.encode(email, forKey: "email")
     aCoder.encode(name, forKey: "name")
     aCoder.encode(apiToken, forKey: "apiToken")
   }
