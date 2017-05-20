f1d43eef4044a12431a840baac8e8a12be1a3efb
Dan Kim <itsdanya@gmail.com>
2017-05-20 16:05:18 +0300

    Create input controllers for User

---
+++ BeaconTracker/InputType.swift
@@ -0,0 +1,21 @@
+//
+//  InputType.swift
+//  BeaconTracker
+//
+//  Created by Dan Kim on 2017-05-20.
+//  Copyright © 2017 Dan Kim. All rights reserved.
+//
+
+import Foundation
+
+enum InputType {
+  case email, name, password
+
+  var title: String {
+    switch self {
+    case .email: return "Email"
+    case .name: return "Name"
+    case .password: return "Password"
+    }
+  }
+}
