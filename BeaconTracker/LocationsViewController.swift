5851dc1d429ddaffb5ded49c6c7dfcec958f2f20
Dan Kim <itsdanya@gmail.com>
2017-05-24 01:23:43 +0300

    Create LocationsViewController

---
+++ BeaconTracker/LocationsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LocationsViewController.swift
+//  BeaconTracker
+//
+//  Created by Dan Kim on 2017-05-24.
+//  Copyright © 2017 Dan Kim. All rights reserved.
+//
+
+import UIKit
+
+class LocationsViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+  }
+
+  // MARK: - Table view data source
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return 0
+  }
+
+}
